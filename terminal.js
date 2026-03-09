f05e9d70c614f85c1e0da58cfdc65d379fd46db4
Niffanni <155296155+Niffanni@users.noreply.github.com>
2026-03-10 09:02:15 +1100

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -1,37 +1,65 @@
+const output = document.getElementById("output")
+const cmd = document.getElementById("cmd")
+
+function print(text){
+output.innerHTML += text + "<br>"
+output.scrollTop = output.scrollHeight
+}
+
+cmd.addEventListener("keydown", function(e){
+
+if(e.key === "Enter"){
+
+let command = this.value.trim().toLowerCase()
+
+print("> " + command)
+
+runCommand(command)
+
+this.value = ""
+
+}
+
+})
+
 function runCommand(command){
 
-if(command==="help"){
+if(command === "help"){
 print("Available commands:")
 print("whoami")
 print("join network")
 print("scan")
 print("map")
 print("summon rabbit")
+return
 }
 
-else if(command==="whoami"){
+if(command === "whoami"){
 print("You are a TwinSignal node.")
+return
 }
 
-else if(command==="join network"){
+if(command === "join network"){
 print("Node accepted into the signal mesh.")
+return
 }
 
-else if(command==="scan"){
+if(command === "scan"){
 print("Scanning nearby nodes...")
 print("1 signal detected.")
+return
 }
 
-else if(command==="map"){
-print("Opening constellation network map...")
+if(command === "map"){
+print("Opening constellation map...")
+return
 }
 
-else if(command==="summon rabbit"){
+if(command === "summon rabbit"){
 print("Glitch rabbit anomaly detected 🐇")
+return
 }
 
-else{
 print("Unknown command")
-}
 
 }
